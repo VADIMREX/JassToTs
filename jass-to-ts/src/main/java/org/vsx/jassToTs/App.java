7b7097800e6b24051cccee6ac0606d5ffc6eeb1d
Вадим <nerzhularthas@gmail.com>
2021-08-21 21:43:16 +0600

    Created Java project

---
+++ jass-to-ts/src/main/java/org/vsx/jassToTs/App.java
@@ -0,0 +1,15 @@
+package org.vsx.jassToTs;
+
+import org.vsx.jass.TokenKind;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("");
+    }
+}
