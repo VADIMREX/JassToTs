692cd14485c87d17ae210d7f1845ef998449dac7
Вадим <nerzhularthas@gmail.com>
2021-09-04 13:58:21 +0600

    Language.cs proceeded

---
+++ jass-to-ts/src/main/java/org/vsx/jassToTs/Language.java
@@ -0,0 +1,8 @@
+package org.vsx.jassToTs;
+
+public class Language {
+    public static final int TypeScript = 0;
+    public static final int TypeScriptDeclaration = 1;
+    public static final int Lua = 2;
+    public static final int GalaxyRaw = 3;
+}
