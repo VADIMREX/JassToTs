c91040beca75750d0940e5cbd2ff4dc4b8382fc2
Вадим <nerzhularthas@gmail.com>
2021-09-05 00:53:59 +0600

    Changes to class Statement   - added new constructors   - replaced type of list Childs

--- jass-to-ts/src/main/java/org/vsx/jass/Statement.java
+++ jass-to-ts/src/main/java/org/vsx/jass/Statement.java
@@ -1,16 +1,24 @@
 package org.vsx.jass;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
-public class Statement
-{
+public class Statement {
     public String Type;
     public Token Start;
     public Statement Parent;
-    public LinkedList<Statement> Childs = new LinkedList<Statement>();
+    public ArrayList<Statement> Childs = new ArrayList<Statement>();
 
     public Statement() {
 
+    }
+
+    public Statement(String type) {
+        this.Type = type;
+    }
+
+    public Statement(String type, Token start) {
+        this.Type = type;
+        this.Start = start;
     }
 
     public Statement(Statement parent, String type, Token start) {
@@ -21,46 +29,48 @@ public class Statement
 
     /**
      * Создать и добавить дочернюю инструкцию
+     * 
      * @param type  тип инструкции
      * @param token токен с которого начинается потомок
      * @return дочернюю инструкцию
      * @see Statement#AddChild
      */
-    public Statement MakeChild(String type, Token token)
-    {
+    public Statement MakeChild(String type, Token token) {
         var child = new Statement(this, type, token);
         Childs.add(child);
         return child;
     }
 
-
     /**
      * Создать и добавить дочернюю инструкцию
-     * @param type тип инструкции
+     * 
+     * @param type  тип инструкции
      * @param token токен с которого начинается потомок
      * @return себя
      * @see Statement#MakeChild
      */
-    public Statement AddChild(String type, Token token)
-    {
+    public Statement AddChild(String type, Token token) {
         MakeChild(type, token);
         return this;
     }
 
     /**
      * Добавить дочернюю инструкцию
+     * 
      * @param child дочерняя инструкция
      * @return себя
      */
-    public Statement AddChild(Statement child)
-    {
+    public Statement AddChild(Statement child) {
         child.Parent = this;
         Childs.add(child);
         return this;
     }
 
     @Override
-    public String toString() { return toString(0); }
+    public String toString() {
+        return toString(0);
+    }
+
     public String toString(int ident) {
         String s = "";
         for (Statement statement : Childs) {
@@ -69,8 +79,8 @@ public class Statement
         return String.format("%s%s %s\n%s", "", Type, Start, s);
         // $"// {"".PadLeft(ident, ' ')}{Type} {Start}\n" +
         // (Childs.Count > 0 ?
-        //     Childs.Select(x => x.ToString(ident + 2)).Aggregate((x, y) => $"{x}{y}") :
-        //     ""
+        // Childs.Select(x => x.ToString(ident + 2)).Aggregate((x, y) => $"{x}{y}") :
+        // ""
         // );2
     }
 }
